2d1416004e228f67e8aa2e74d29d5efeda7d98f4
EC2 Default User <ec2-user@ip-172-31-13-200.us-east-2.compute.internal>
2020-11-19 07:10:37 +0000

    Generate homes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'homes#index'
+  get 'homes/index', to: 'homes#index'
+  get 'homes/about', to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
