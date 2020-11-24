f8471cd5a177697b58d87a13c2047f1bc096ce11
EC2 Default User <ec2-user@ip-172-31-13-200.us-east-2.compute.internal>
2020-11-24 10:59:08 +0000

    Show thumbnail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def show_thumbnail(user, width=100, height=100)
+    if user.thumbnail_id
+      img = image_tag(attachment_url(current_user, :thumbnail, :fill, width, height))
+    else
+      img = image_tag('no_image.jpg', width: width, height: height)
+    end
+    img
+  end
 end
